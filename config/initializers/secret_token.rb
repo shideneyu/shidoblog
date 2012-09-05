48349a528cc173a5cc9e705f7b0a5f2e8964c964
Sidney Sissaoui <shideneyu@gmail.com>
2012-09-05 14:21:15 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shidoblog::Application.config.secret_token = '73ac29738fcd78c0bc2ef3615ac240dd3e073195c578fc68e86afd553f7a19f82d9fd0551fe2e256e983eb645a7e2720b65ea44d7ca4cb28ed52e17dfa317210'
